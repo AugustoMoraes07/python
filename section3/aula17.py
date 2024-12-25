b62fa92e40b0f6db4d5eed49505aaa801d4474a5
Augusto <augustofelipemf45@gmail.com>
2024-12-25 17:17:59 -0300

    news exercices

---
+++ section3/aula17.py
@@ -0,0 +1,14 @@
+# if/else/elif
+# se/senao/senaose
+condicao = True
+condicao1 = False
+if condicao:
+    print('Este é o código if')
+elif not condicao:
+    print('Este é o código elif')
+elif condicao1:
+    ...
+else:
+    print('Este é o código else')
+
+print('Fora das condicionais')    
--- section3/formatacao-strings/format.py
+++ section3/formatacao-strings/format.py
@@ -2,5 +2,5 @@ name = input('What\'s your name? ')
 job = input('What\'s your job? ')
 city = input('Are you from ? ')
 msg = 'I\'m {0}, my job is {1}. I live in {2}'
-format = msg.format(name,job,city)
-print(format)
+format_msg = msg.format(name,job,city)
+print(format_msg)
